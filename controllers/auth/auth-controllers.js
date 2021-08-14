05831a796c9527f89142a0f314939a6e85de9999
boybenson <ybenson96@gmail.com>
2021-08-14 01:00:26 +0000

    feat: done with the signin endpoint

--- controllers/auth/auth-controllers.js
+++ controllers/auth/auth-controllers.js
@@ -37,13 +37,45 @@ export const POST_SIGNUP_USER = async (req, res, next) => {
   }
 };
 
-export const POST_SIGNIN_USER = (req, res, next) => {
+export const POST_SIGNIN_USER = async (req, res, next) => {
+  const { email, password } = req.body;
   try {
-    res.json({ message: "This is the signin route" });
+    const isUser = await userModel.findOne({ email });
+
+    if (!isUser) {
+      const err = new Error();
+      err.message = "incorrect email address";
+      err.status = 404;
+      next(err);
+      return;
+    }
+
+    const user = await userModel.login(email, password);
+
+    // checkout for incorrect password
+    if (user.status === 403) {
+      const err = new Error();
+      err.message = "incorrect password";
+      err.status = 403;
+      next(err);
+      return;
+    }
+
+    const accessToken = generateToken(user._id);
+
+    res.status(200).json({
+      username: user.userName,
+      email: user.email,
+      userId: user._id,
+      phone: user.phone,
+      role: user.role,
+      accessToken,
+    });
   } catch (error) {
-    let err = new Error(error.message);
+    const err = new Error();
     err.message = error.message;
     err.status = 500;
     next(err);
+    return;
   }
 };
